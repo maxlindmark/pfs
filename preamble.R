6e437b869444b23fb51f9b61335360deed3f3098
Max Lindmark <max.lindmark@slu.se>
2022-10-27 20:37:01 +0200

    first commit!

---
+++ preamble.R
@@ -0,0 +1,38 @@
+xaringanthemer::style_mono_accent(
+  # base_color = "#202020",
+  # header_font_google = xaringanthemer::google_font("Raleway"),
+  # text_font_google = xaringanthemer::google_font("Open Sans"),
+  # code_font_google = xaringanthemer::google_font("Fira Mono"),
+  # title_slide_background_image = "images/big_fish.png",
+  # title_slide_background_size = "30%",
+  # title_slide_background_position = "50% 100%",
+  base_font_size = "20px",
+  header_h1_font_size = "1.9rem",
+  text_font_size = "1.5rem",
+  code_font_size = "1.1rem",
+  link_color = "#0047AB",
+)
+
+knitr_opts <- list(
+  message = FALSE,
+  warning = FALSE,
+  collapse = TRUE,
+  comment = "#>",
+  fig.width = 7,
+  dpi = 300,
+  out.width = "700px",
+  fig.asp = 1 / 1.618,
+  cache = TRUE,
+  autodep = TRUE,
+  cache.comments = TRUE,
+  fig.align = "center",
+  echo = FALSE
+)
+
+
+
+library(ggplot2)
+library(dplyr)
+options(ggplot2.continuous.colour = "viridis")
+options(ggplot2.continuous.fill = "viridis")
+ggplot2::theme_set(ggplot2::theme_minimal())
